f25cd90bdf46f028ddc2959d264198393a50996f
Pasindu Malinda Waidyarathne <108586125+PasinduWaidyarathna@users.noreply.github.com>
2024-12-10 09:29:03 +0530

    initial commit

---
+++ src/main/java/com/example/rentmate_backend/RentmateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentmate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentmateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentmateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rentmate_backend/RentmateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentmate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentmateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
