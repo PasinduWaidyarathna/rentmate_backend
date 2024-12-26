e5bf901447dd86e4f094ea9d3c276636af26d196
Pasindu Malinda Waidyarathne <108586125+PasinduWaidyarathna@users.noreply.github.com>
2024-12-26 14:58:23 +0530

    add crossorigin for user

--- src/main/java/com/example/rentmate_backend/controller/UserController.java
+++ src/main/java/com/example/rentmate_backend/controller/UserController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/user")
+@CrossOrigin(origins = "*")
 @Tag(name="UserController",description="To perform operations on Users")
 public class UserController {
 
