15d6cdb0c9a13412593deb626809e46649c404d5
Pasindu Malinda Waidyarathne <108586125+PasinduWaidyarathna@users.noreply.github.com>
2025-02-06 21:05:18 +0530

    remove unneccesary spaces

--- src/main/java/com/example/rentmate_backend/repository/CategoryRepository.java
+++ src/main/java/com/example/rentmate_backend/repository/CategoryRepository.java
@@ -1,10 +1,6 @@
 package com.example.rentmate_backend.repository;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
 import com.example.rentmate_backend.model.Category;
 
 public interface CategoryRepository extends MongoRepository<Category, String> {
