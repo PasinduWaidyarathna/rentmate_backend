3356b821a08a18d311bb04a1f9c3489a7c418ef9
Pasindu Malinda Waidyarathne <108586125+PasinduWaidyarathna@users.noreply.github.com>
2025-03-05 21:19:17 +0530

    add authorize to CategoryController

--- src/main/java/com/example/rentmate_backend/controller/CategoryController.java
+++ src/main/java/com/example/rentmate_backend/controller/CategoryController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,6 +27,7 @@ public class CategoryController {
             description = "It is used to save Category object in database"
     )
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Category> createCategory(@RequestBody Category category) {
         Category createdCategory = categoryService.createCategory(category);
         return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
@@ -37,6 +39,7 @@ public class CategoryController {
             description = "Endpoint to fetch a list of all Categories from the database"
     )
     @GetMapping
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<List<Category>> getAllCategories() {
         List<Category> categories = categoryService.getAllCategories();
         return new ResponseEntity<>(categories, HttpStatus.OK);
@@ -48,6 +51,7 @@ public class CategoryController {
             description = "Endpoint to retrieve a specific Category using their unique identifier"
     )
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<Category> getCategoryById(@PathVariable String id) {
         Category category = categoryService.getCategoryById(id);
         if (category == null) {
@@ -62,6 +66,7 @@ public class CategoryController {
             description = "Endpoint to retrieve a specific Category using their name"
     )
     @GetMapping("/name/{name}")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<Category> getCategoryByName(@PathVariable String name) {
         return ResponseEntity.ok(categoryService.getCategoryByName(name));
     }
@@ -72,6 +77,7 @@ public class CategoryController {
             description = "Endpoint to update an existing category's details using their ID"
     )
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Category> updateCategory(@PathVariable String id, @RequestBody Category category) {
         Category updatedCategory = categoryService.updateCategory(id, category);
         if (updatedCategory == null) {
@@ -86,6 +92,7 @@ public class CategoryController {
             description = "Endpoint to remove a specific Category from the database using their unique identifier"
     )
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteCategory(@PathVariable String id) {
         categoryService.deleteCategory(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -97,6 +104,7 @@ public class CategoryController {
             description = "Endpoint to remove a specific Category from the database using their name"
     )
     @DeleteMapping("/delete/{name}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> deleteCategoryByName(@PathVariable String name) {
         categoryService.deleteCategoryByName(name);
         return ResponseEntity.ok("Category '" + name + "' deleted successfully.");
